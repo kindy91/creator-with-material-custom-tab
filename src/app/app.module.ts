ec993a9cd1bda2d18e951cf348f5085622eb8978
a.elkindy <a.elkindy@onventis.de>
2023-08-29 09:40:08 +0200

    change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
+import { MatInputModule } from '@angular/material/input';
 import { BrowserModule } from '@angular/platform-browser';
+import { SurveyCreatorModule } from 'survey-creator-angular';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CustomTabComponent } from './custom-tab/custom-tab.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomTabComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    SurveyCreatorModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
